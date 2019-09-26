c07b9f27fd5973426fce6ad4bfd78ffc5dfc5c29
huangguizhao <2678383176@qq.com>
2019-09-26 15:40:12 +0800

    master commit 3

--- src/main/java/com/qianfeng/gittest/controller/UserController.java
+++ src/main/java/com/qianfeng/gittest/controller/UserController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     public static void main(String[] args){
-
+        System.out.println("master");
     }
 }
