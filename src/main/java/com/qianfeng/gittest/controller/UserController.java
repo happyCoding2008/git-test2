2e84636dae6fa5dcaca86658222b1c551b1cfc37
huangguizhao <2678383176@qq.com>
2019-09-26 15:36:55 +0800

    dev1 commit

--- src/main/java/com/qianfeng/gittest/controller/UserController.java
+++ src/main/java/com/qianfeng/gittest/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.qianfeng.gittest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author huangguizhao
  */
+@Controller
+@RequestMapping("user")
 public class UserController {
+
+    public static void main(String[] args){
+
+    }
 }
